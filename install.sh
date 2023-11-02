cc8a9ffe13a6ffebdcf7255807cb7af705df6625
Bruno Costa <brunovasquescosta@gmail.com>
2023-11-02 21:42:56 +0000

    Adds install file to install app

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#Makes the express app
+express --view=pug
+npm i
+
+#Add necessary express dependencies
+mkdir -p {src/js,src/scss} 
+touch {src/js/main.js,src/scss/style.scss}
+cp views/layout.pug views/webpack.pug
+echo -n "// Import our custom CSS\nimport '../scss/styles.scss'\n\n// Import all of Bootstrap's JS\nimport * as bootstrap from 'bootstrap'"
+npm run build
