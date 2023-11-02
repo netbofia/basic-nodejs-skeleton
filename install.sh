b981a8bcc14c34d907d4cdccf2c338ae85b0bac4
Bruno Costa <brunovasquescosta@gmail.com>
2023-11-02 23:12:38 +0000

    Add the ci with all necessary steps

--- install.sh
+++ install.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 #Makes the express app
 express --view=pug -f --git
+npm i
+
 npm i @popperjs/core bootstrap cookieconsent vue vue-select jquery --save
 npm i autoprefixer chai css-loader html-webpack-plugin html-webpack-pug-plugin nodemon postcss-loader sass sass-loader source-map-loader style-loader webpack webpack-cli mocha chai --save-dev
-npm i
+npm i request --save-dev
+
 npm pkg set scripts.build="webpack"
 npm pkg set scripts.install="bash ./install.sh" 
 npm pkg set scripts.test="mocha" 
-npm i request --save-dev
+
 #Add necessary express dependencies
 mkdir -p {src/js,src/scss} 
 touch {src/js/main.js,src/scss/style.scss}
 cp views/layout.pug views/webpack.pug
 echo -n "// Import our custom CSS\nimport '../scss/styles.scss'\n\n// Import all of Bootstrap's JS\nimport * as bootstrap from 'bootstrap'" > src/js/main.js
-npm run build
